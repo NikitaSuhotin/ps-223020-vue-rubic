a9fdf5d184a29125612f04859140b6cbc7650947
NikitaSuhotin <nox.dregon@yandex.ru>
2021-11-07 17:53:31 +0300

    Add prifile

--- src/router/index.js
+++ src/router/index.js
@@ -88,6 +88,20 @@ export const constantRoutes = [
         meta: { title: 'Form', icon: 'form' }
       }
     ]
+  },
+  {
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
   }
 ]
 
@@ -170,7 +184,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
